75a23f3b112c965ddd97157a11dc96c9c8d17c3c
martinghunt <martin.g.hunt@gmail.com>
2018-01-29 06:56:36 -0800

    New options --max_snps_per_cluster --max_ref_len

--- minos/tasks/adjudicate.py
+++ minos/tasks/adjudicate.py
@@ -9,6 +9,8 @@ def run(options):
         max_read_length=options.max_read_length,
         read_error_rate=options.read_error_rate,
         overwrite_outdir=options.force,
+        max_snps_per_cluster=options.max_snps_per_cluster,
+        max_ref_len=options.max_ref_len,
     )
     adj.run()
 
