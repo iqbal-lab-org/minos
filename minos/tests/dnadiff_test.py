73abd1d78a08efd2a001d699ba66da6c227f06fe
Martin Hunt <martinghunt@gmail.com>
2018-02-07 11:55:01 +0000

    Fix off by one errors

--- minos/tests/dnadiff_test.py
+++ minos/tests/dnadiff_test.py
@@ -74,7 +74,7 @@ def write_fasta_files(ref_fa, qry_fa):
     expected_vcf_records = {
         'qry.snp_indel': [
             vcf_record.VcfRecord('qry.snp_indel\t100\t.\tA\tG\t.\t.\tSVTYPE=DNADIFF_SNP\tGT\t1/1'),
-            vcf_record.VcfRecord('qry.snp_indel\t194\t.\tC\tCGATTC\t.\t.\tSVTYPE=DNADIFF_INS\tGT\t1/1'),
+            vcf_record.VcfRecord('qry.snp_indel\t195\t.\tA\tAGATTC\t.\t.\tSVTYPE=DNADIFF_INS\tGT\t1/1'),
             vcf_record.VcfRecord('qry.snp_indel\t395\t.\tAACGT\tA\t.\t.\tSVTYPE=DNADIFF_DEL\tGT\t1/1'),
         ],
         'qry.snp_indel.2': [
