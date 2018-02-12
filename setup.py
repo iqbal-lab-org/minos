e7e00ec07c6ef98096e748766f39d43f41d28a73
Martin Hunt <martin.g.hunt@gmail.com>
2018-02-12 20:18:27 +0000

    Version bump 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bio-minos',
-    version='0.0.1',
+    version='0.0.2',
     description='Variant call adjudication',
     packages = find_packages(),
     author='Martin Hunt',
