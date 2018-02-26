65521eec5681460b76de68108c3e389acdf5268d
Martin Hunt <martin.g.hunt@gmail.com>
2018-02-26 06:16:28 -0800

    Handle when no variants are input to adjudicate

--- minos/tests/adjudicator_test.py
+++ minos/tests/adjudicator_test.py
@@ -27,3 +27,22 @@ class TestAdjudicator(unittest.TestCase):
         self.assertTrue(os.path.exists(adj.clustered_vcf))
         shutil.rmtree(outdir)
 
+
+    def test_run_empty_vcf_input_files(self):
+        '''test run when input files have no variants'''
+        outdir = 'tmp.adjudicator.out'
+        if os.path.exists(outdir):
+            shutil.rmtree(outdir)
+
+        ref_fasta = os.path.join(data_dir, 'run.ref.fa')
+        reads_file = os.path.join(data_dir, 'run.bwa.bam')
+        vcf_files =  [os.path.join(data_dir, x) for x in ['run.calls.empty.1.vcf', 'run.calls.empty.2.vcf']]
+        adj = adjudicator.Adjudicator(outdir, ref_fasta, [reads_file], vcf_files)
+        with self.assertRaises(adjudicator.Error):
+            adj.run()
+        self.assertTrue(os.path.exists(outdir))
+        self.assertTrue(os.path.exists(adj.log_file))
+        self.assertFalse(os.path.exists(adj.final_vcf))
+        self.assertFalse(os.path.exists(adj.gramtools_outdir))
+        self.assertTrue(os.path.exists(adj.clustered_vcf))
+        shutil.rmtree(outdir)
