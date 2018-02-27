cc628ad87ad0f34e92acc3a72d9350b94b7a0b9e
Martin Hunt <martin.g.hunt@gmail.com>
2018-02-27 07:11:09 -0800

    New method _load_input_data_tsv

--- minos/multi_sample_pipeline.py
+++ minos/multi_sample_pipeline.py
@@ -19,6 +19,28 @@ class MultiSamplePipeline:
         self.min_large_ref_length = min_large_ref_length
 
 
+    @classmethod
+    def _load_input_data_tsv(cls, infile):
+        logging.info('Start reading file ' + infile)
+        data = []
+        with open(infile) as f:
+            for line in f:
+                try:
+                    vcf_file, reads_file = line.rstrip().split('\t')
+                except:
+                    raise Error('Bad line in input TSV file: ' + line.rstrip())
+
+                if not(os.path.exists(vcf_file)):
+                    raise Error('VCF file not found: ' + vcf_file)
+                if not(os.path.exists(reads_file)):
+                    raise Error('Reads file not found: ' + vcf_file)
+
+                data.append((os.path.abspath(vcf_file), os.path.abspath(reads_file)))
+
+        logging.info('Finish reading file ' + infile + '. Loaded ' + str(len(data)) + 'samples')
+        return data
+
+
     def _write_nextflow_script(self):
         pass
 
--- minos/tests/multi_sample_pipeline_test.py
+++ minos/tests/multi_sample_pipeline_test.py
@@ -5,11 +5,46 @@ import unittest
 
 from minos import multi_sample_pipeline
 
-modules_dir = os.path.dirname(os.path.abspath(multi_sample_pipeline.py.__file__))
+modules_dir = os.path.dirname(os.path.abspath(multi_sample_pipeline.__file__))
 data_dir = os.path.join(modules_dir, 'tests', 'data', 'multi_sample_pipeline.py')
 
 class TestMultiSamplePipeline(unittest.TestCase):
-    def test_run(self):
-        '''test run'''
-        self.assertTrue(False)
+    def test_load_input_data_tsv(self):
+        '''test _load_input_data_tsv'''
+        # Using absolute paths, so can't easily have
+        # pre-made input files. Hence make them now
+        prefix = 'tmp.multi_sample_pipeline_load_input_data_tsv'
+        tsv_file = prefix + '.data.tsv'
+        vcf1 = prefix + '.sample1.vcf'
+        vcf2 = prefix + '.sample2.vcf'
+        reads1 = prefix + '.sample1.reads'
+        reads2 = prefix + '.sample2.reads'
+        for filename in [tsv_file, vcf1, vcf2, reads1, reads2]:
+            try:
+                os.unlink(filename)
+            except:
+                pass
 
+        with open(tsv_file, 'w') as f:
+            print(vcf1, reads1, sep='\t', file=f)
+            print(vcf2, reads2, sep='\t', file=f)
+
+        for filename in [vcf1, vcf2, reads1]:
+            with open(filename, 'w'):
+                pass
+            with self.assertRaises(multi_sample_pipeline.Error):
+                multi_sample_pipeline.MultiSamplePipeline._load_input_data_tsv(tsv_file)
+
+        with open(reads2, 'w'):
+            pass
+
+        got = multi_sample_pipeline.MultiSamplePipeline._load_input_data_tsv(tsv_file)
+        expected = [
+            (os.path.abspath(vcf1), os.path.abspath(reads1)),
+            (os.path.abspath(vcf2), os.path.abspath(reads2)),
+        ]
+
+        self.assertEqual(expected, got)
+
+        for filename in [tsv_file, vcf1, vcf2, reads1, reads2]:
+            os.unlink(filename)
