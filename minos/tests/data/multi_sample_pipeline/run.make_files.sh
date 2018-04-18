3b2d3f62b8775d525c4e95598ac8ec9c5dbe75ec
Martin Hunt <martin.g.hunt@gmail.com>
2018-04-18 12:09:04 -0700

    Make sorted BAM files for testing splitting in pipeline

--- minos/tests/data/multi_sample_pipeline/run.make_files.sh
+++ minos/tests/data/multi_sample_pipeline/run.make_files.sh
@@ -10,9 +10,9 @@ for i in 1 2; do
     reads1=$pre.reads.$i.1.fq
     reads2=$pre.reads.$i.2.fq
     fastaq to_perfect_reads --seed 42 $pre.ref.$i.fa -  230 1 20 100 | fastaq deinterleave - $reads1 $reads2
-    bwa mem -R "@RG\tID:sample.$i\tSM:sample.$i" $ref $reads1 $reads2 | samtools sort -o tmp.$$.bam
-    samtools mpileup -ugf $ref tmp.$$.bam | bcftools call -vm -O v -o $pre.calls.$i.vcf
-    rm tmp.$$.bam
+    bwa mem -R "@RG\tID:sample.$i\tSM:sample.$i" $ref $reads1 $reads2 | samtools sort -o $pre.reads.$i.sorted.bam
+    samtools mpileup -ugf $ref $pre.reads.$i.sorted.bam | bcftools call -vm -O v -o $pre.calls.$i.vcf
+    samtools index $pre.reads.$i.sorted.bam
 done
 
 rm $ref.*
