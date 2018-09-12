5d6fbc6b31873b2e33ac2d3fd71f7f31a7e7cafc
Rachel Norris <rmnorris@well.ox.ac.uk>
2018-09-12 09:49:01 +0100

    handle nanopolish vcf

--- minos/mapping_based_verifier.py
+++ minos/mapping_based_verifier.py
@@ -218,6 +218,9 @@ class MappingBasedVerifier:
                             vcf_record.set_format_key_value('GT_CONF',str(float(likelihoods[0]) - float(likelihoods[1])))
                         else:
                             vcf_record.set_format_key_value('GT_CONF', str(float(likelihoods[int(genotypes[0])]) - float(likelihoods[0])))
+                    if 'SupportFraction' in vcf_record.INFO.keys() and 'GT_CONF' not in vcf_record.FORMAT.keys():
+                        vcf_record.set_format_key_value('GT_CONF',
+                                                        str(vcf_record.INFO['SupportFraction']*100))
                     print(vcf_record, file=f)
 
 
