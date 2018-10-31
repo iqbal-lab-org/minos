6a10c1fa1a866506b6120f8fa0c740dc8e7ff400
Rachel Norris <rmnorris@well.ox.ac.uk>
2018-10-31 11:50:53 +0000

    add second test

--- minos/tests/dnadiff_mapping_based_verifier_test.py
+++ minos/tests/dnadiff_mapping_based_verifier_test.py
@@ -29,3 +29,18 @@ class TestDnadiffMappingBasedVerifier(unittest.TestCase):
         self.assertTrue(filecmp.cmp(expected_out2, tmp_out2, shallow=False))
         os.unlink(tmp_out1)
         os.unlink(tmp_out2)
+
+    def test_parse_sam_file_and_vcf(self):
+        samfile = os.path.join(data_dir, 'test.1.sam')
+        vcffile = os.path.join(data_dir, 'sample1a.vcf')
+
+        flank = 5
+        allow_mismatches = False
+        found, gt_conf = dnadiff_mapping_based_verifier.DnadiffMappingBasedVerifier._parse_sam_file_and_vcf(samfile, vcffile, flank, allow_mismatches)
+
+        exp_found = ['1','1','0','0','1','1','1']
+        exp_gt_conf = [42.42, 42.42, None, None, 42.42, 32.32, None]
+        self.assertEqual(expect_found, found)
+        self.assertEqual(expect_gt_conf, gt_conf)
+
+
