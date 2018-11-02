a01dfec13b3b1574663303389c781a5d99215391
Rachel Norris <rmnorris@well.ox.ac.uk>
2018-11-02 12:57:51 +0000

    debugging

--- minos/dnadiff_mapping_based_verifier.py
+++ minos/dnadiff_mapping_based_verifier.py
@@ -341,8 +341,9 @@ class DnadiffMappingBasedVerifier:
                 vcf_reader = pysam.VariantFile(vcffile)
                 for i, vcf_record in enumerate(vcf_reader.fetch(ref_name, int(expected_start) + flank_length - 1, int(expected_start) + flank_length)):
                     print(vcf_record)
-                    print (i, " ", vcf_pos_index)
+                    print(i, " ", vcf_pos_index)
                     if i == vcf_pos_index:
+                        print('GT' in vcf_record.FORMAT, " ", len(set(vcf_record.FORMAT['GT'].split('/'))))
                         if 'GT' in vcf_record.FORMAT and len(set(vcf_record.FORMAT['GT'].split('/'))) == 1:
                             print(vcf_record.FORMAT['GT'].split('/')[0], allele_index)
                             if allele_index == vcf_record.FORMAT['GT'].split('/')[0]:
