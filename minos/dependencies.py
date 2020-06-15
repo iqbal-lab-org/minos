ba7d6b2d4c5618230352887bce91927798822bcd
Martin Hunt <martin.g.hunt@gmail.com>
2020-06-15 12:16:48 +0000

    Remove pymummer

--- minos/dependencies.py
+++ minos/dependencies.py
@@ -50,7 +50,7 @@ def find_python_packages():
     package_name => (version, path).
     Values will be None if package not found"""
     packages = {}
-    for package in ["minos", "pyfastaq", "pymummer", "pysam"]:
+    for package in ["minos", "pyfastaq", "pysam"]:
         try:
             exec("import " + package)
             version = eval(package + ".__version__")
